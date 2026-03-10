f527e12f2ca848706a7f3ecdb5157d02af5c0083
wangshu <2560334673@qq.com>
2026-03-10 18:12:52 +0800

    commit

--- js/douyin.js
+++ js/douyin.js
@@ -6,17 +6,33 @@ console.log("Only for learning to use.")
 
 let likeTimer;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let {timeout} = request;
-    timeout ??= 100;
+function startLike(timeout) {
+    timeout ??= 100
+    likeTimer && clearInterval(likeTimer);
     const likeButtons = document.getElementById("LikeLayout").children;
     if (likeButtons && likeButtons.length > 0) {
-        clearInterval(likeTimer);
         const likeBtn = likeButtons[0];
         likeTimer = setInterval(() => {
             likeBtn.click()
             likeBtn.click()
         }, timeout);
-        sendResponse({fromcontent: "开始点赞"});
+    }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    let {command, data} = request;
+    switch (command) {
+        case 'start':
+            startLike(data?.timeout);
+            sendResponse({fromcontent: "开始点赞"});
+            break;
+        case 'stop':
+            clearInterval(likeTimer);
+            sendResponse({fromcontent: "暂停点赞"});
+            break;
+        case 'update-timeout':
+            startLike(data?.timeout);
+            sendResponse({fromcontent: "更新点赞间隔时间"});
+            break;
     }
 })
--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,43 @@
 const startLikeBtn = document.getElementById("start-like");
+const stopLikeBtn = document.getElementById("stop-like");
 const timeoutRange = document.getElementById('timeout-range');
 const timeoutValueSpan = document.getElementById('timeout-value');
 if (startLikeBtn && timeoutRange && timeoutValueSpan) {
     timeoutValueSpan.innerText = `${timeoutRange.value} ms`;
     timeoutRange.addEventListener('change', (e) => {
         timeoutValueSpan.innerText = `${e.target.value} ms`;
+        const timeout = e.target.value ?? 100;
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                command: "update-timeout",
+                data: {
+                    timeout
+                }
+            }, (response) => {
+                // window.close();
+            });
+        });
     })
     startLikeBtn.addEventListener('click', (e) => {
         const timeout = timeoutRange.value ?? 100;
         chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, {timeout}, (response) => {
-                window.close();
+            chrome.tabs.sendMessage(tabs[0].id, {
+                command: "start",
+                data: {
+                    timeout
+                }
+            }, (response) => {
+                // window.close();
+            });
+        });
+    })
+    stopLikeBtn.addEventListener('click', (e) => {
+        const timeout = timeoutRange.value ?? 100;
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                command: "stop"
+            }, (response) => {
+                // window.close();
             });
         });
     })
